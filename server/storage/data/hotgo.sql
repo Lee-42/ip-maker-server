f0978dd644733b2f6b566f415d8cc31c4271159e
Lee <1725210731@qq.com>
2026-01-30 18:19:05 +0800

    feat: add hg_app_user table sql

--- server/storage/data/hotgo.sql
+++ server/storage/data/hotgo.sql
@@ -6205,3 +6205,34 @@ ALTER TABLE `hg_test_category`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `hg_app_user`
+--
+
+CREATE TABLE IF NOT EXISTS `hg_app_user` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `username` varchar(50) NOT NULL DEFAULT '' COMMENT '用户名',
+    `nickname` varchar(100) DEFAULT '' COMMENT '昵称',
+    `password_hash` varchar(255) NOT NULL DEFAULT '' COMMENT '密码哈希',
+    `salt` varchar(10) NOT NULL COMMENT '密码盐',
+    `mobile` varchar(20) DEFAULT '' COMMENT '手机号',
+    `email` varchar(100) DEFAULT '' COMMENT '邮箱',
+    `avatar` varchar(500) DEFAULT '' COMMENT '头像URL',
+    `sex` tinyint(1) DEFAULT '0' COMMENT '性别',
+    `birthday` date DEFAULT NULL COMMENT '生日',
+    `last_login_at` datetime DEFAULT NULL COMMENT '最后登录时间',
+    `last_login_ip` varchar(50) DEFAULT '' COMMENT '最后登录IP',
+    `status` tinyint(1) DEFAULT '1' COMMENT '状态',
+    `remark` text COMMENT '备注',
+    `created_at` datetime DEFAULT NULL COMMENT '创建时间',
+    `updated_at` datetime DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uniq_username` (`username`),
+    KEY `idx_mobile` (`mobile`),
+    KEY `idx_email` (`email`),
+    KEY `idx_status` (`status`),
+    KEY `idx_created_at` (`created_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='应用_普通用户';
